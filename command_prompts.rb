7fae12bc86b05457468e970b952d50e6eefd34f6
babyshoes <dianecai@gmail.com>
2015-08-06 16:32:57 -0400

    laid out

---
+++ command_prompts.rb
@@ -0,0 +1,111 @@
+require_relative 'index-scrape'
+require 'pry'
+
+class Interface
+
+  attr_reader 
+
+  def initialize
+    @students_list = Index.new
+    all_students = @students_list.students
+    greeting
+  end
+
+  def greeting
+    puts "Every Story Has Heroes.\nWe're 26 people on a journey to learn to love code.\n\nType index or the full name of whomever you are stalking.\nAlternately, find a particular twitter, bio, or blog if you are just here for business."
+    index_or_other
+  end
+
+  def index_or_other
+    input = gets.chomp.downcase
+    if input == 'index'
+      index
+    if input == 'twitter'
+      hotkey(twitter)
+    if input == 'bio' || input == 'biography'
+      hotkey(bio)
+    if input == 'blog'
+      hotkey(blog)
+    elsif all_students.any? {|n, o| n.downcase == input}
+      find(input)
+    else
+      puts "If you don't know the full name of the person you are stalking, you soon will. Type index."
+      index_or_other
+    end
+  end
+
+  def index
+    all_students.each {|name, object| puts "#{name} - #{object.tag}" }
+    puts "\n\n"
+    look_up
+  end
+
+  def look_up
+    puts 'Who do you want to look up?'
+    name = gets.chomp.downcase
+    find(name)
+  end
+
+  def find(name)
+    if all_students.any? {|n, o| n.downcase == name}
+      puts "this happens"
+      #instantiates profile
+      all_students[name.downcase].open_profile
+      puts "Name: #{name}
+      \nBiography: #{all_students[name.downcase].biography}
+      \nEducation: #{all_students[name.downcase].education}
+      \nWork: 
+      \nGithub:
+      \nTreehouse:
+      \nCodeSchool:
+      \nCoderwall:
+      \nBlogs:
+      \nPersonal projects:
+      \nFavorite cities:
+      \nFavorite sites:
+      \nPersonal projects:
+      \nPicture: 
+      \n"
+    else
+      puts "No srsly tho."
+      look_up
+    end 
+  end
+
+  def hotkey(item)
+    puts "Whose?"
+    name = gets.chomp.downcase
+    case item
+      when item == "twitter"
+        puts all_students[name.downcase].twitter
+       when item == "bio"
+        puts all_students[name.downcase].bio
+      when item == "blog"
+        puts all_students[name.downcase].blog
+    end   
+  end
+
+  # def twitter
+  #   puts "Whose twitter?"
+  #   name = gets.chomp.downcase
+  #   puts all_students[name.downcase].twitter
+  # end
+
+  # def bio
+  #   puts "Whose bio?"
+  #   name = gets.chomp.downcase
+  #   puts all_students[name.downcase].bio
+  # end
+
+  # def blog
+  #   puts "Whose blog, bub?"
+  #   name = gets.chomp.downcase
+  #   puts all_students[name.downcase].blog
+  # end
+
+end
+
+
+Interface.new
+
+
--- index-scrape.rb
+++ index-scrape.rb
@@ -21,10 +21,11 @@ class Student
 end
 
 class Index
+  attr_accessor :students
   def initialize
     html=open('http://web0715.students.flatironschool.com/')
     index=Nokogiri::HTML(html)
-    students={}
+    @students={}
     index.search("li.home-blog-post").each do |post|
       name=post.search("div.blog-title a").text
       url=post.search("div.blog-title a").attr("href").value
@@ -32,8 +33,8 @@ class Index
       excerpt=post.search("div.excerpt").text.strip
       photo=post.search("div.blog-thumb a img").attr('src').value
       #puts "#{name} #{url} #{tag} #{excerpt} #{photo}"
-      students[name]=Student.new(name,url,tag,excerpt,photo)
+      @students[name]=Student.new(name,url,tag,excerpt,photo)
     end
-    students
+    @students
   end
 end
