18be987fb942bebacb5c19829a96cf35ebc05308
Drew Price <drewprice@users.noreply.github.com>
2015-08-06 17:11:58 -0400

    Add require statements

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,14 @@
+# Gems
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+
+# Student
+require_relative '../lib/student'
+
+# Profile
+require_relative '../lib/profile'
+require_relative '../lib/profile/about'
+require_relative '../lib/profile/favorites'
+require_relative '../lib/profile/recently'
+require_relative '../lib/profile/social'
